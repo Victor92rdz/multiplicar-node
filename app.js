f23d919347922c506c75b095cba6a5b17fb72b11
Victor <victor.rod92@hotmail.com>
2020-12-04 09:07:39 -0600

    Commit inicial

---
+++ app.js
@@ -0,0 +1,50 @@
+const argv = require('yargs')
+    .command('listar', 'imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .command('crear', 'genera un archivo con la tabla de multiplicar', {
+        base: {
+            demand: true,
+            alias: 'b'
+        },
+        limite: {
+            alias: 'l',
+            default: 10
+        }
+    })
+    .argv;
+
+const { crearArchivo, listarTabla } = require('./multiplicar/mutilplicar');
+
+let comando = argv._[0];
+switch (comando) {
+    case 'listar':
+        listarTabla(argv.base, argv.limite);
+        break;
+
+    case 'crear':
+        console.log('crear');
+        crearArchivo(argv.base, argv.limite)
+            .then(archivo => console.log(`Archivo creado: ${ archivo }`))
+            .catch(e => console.log(e));
+        break;
+
+    default:
+        console.log('Comando no reconocido');
+}
+
+
+
+// console.log(process);
+
+/* let parametro = argv[2];
+let base = parametro.split('=')[1] */
+
+//console.log(argv.base);
